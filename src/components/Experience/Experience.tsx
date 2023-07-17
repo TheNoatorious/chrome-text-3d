1edff94ecc7e61fe33f173178795357544c92798
thenoatorious <noatimisela@gmail.com>
2023-07-17 03:57:24 +0200

    Add attributes to 3D text

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Center } from "@react-three/drei";
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import CustomText from "../Text/CustomText";
+import CustomText from "../CustomText/CustomText";
 
 const Experience = () => {
     return (
@@ -9,8 +10,18 @@ const Experience = () => {
             <Perf position="top-left" />
 
             <OrbitControls makeDefault />
-
-            <CustomText />
+            <Center>
+                <CustomText
+                    font="./fonts/canterbury-regular.json"
+                    size={0.75}
+                    height={0.2}
+                    curveSegments={12}
+                    bevelEnabled
+                    bevelThickness={0.02}
+                    bevelOffset={0}
+                    bevelSegments={5}
+                />
+            </Center>
         </>
     );
 };
