aaed8723d84f39b83e713ce35aa776d3976721df
thenoatorious <noatimisela@gmail.com>
2023-07-18 15:41:30 +0200

    Add particle rotation

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import { OrbitControls, Center, meshBounds } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 import CustomText from "../CustomText/CustomText";
 import Diamond from "../Diamond/Diamond";
 
 const Experience = () => {
+    const particlesGroup: any = useRef();
+
+    /**
+     * @param {int} state Access to the renderer
+     * @param {int} delta Time between frames
+     * @returns {int} The rotation
+     **/
+    useFrame((state, delta) => {
+        for (const particle of particlesGroup.current.children) {
+            particle.rotation.y += delta * 0.1;
+        }
+    });
+
     return (
         <>
             <Perf position="top-left" />
@@ -22,22 +36,24 @@ const Experience = () => {
                     bevelSegments={5}
                 />
             </Center>
-            {[...Array(100)].map((value, index) => (
-                <Diamond
-                    key={index}
-                    rotation={[
-                        Math.random() * Math.PI,
-                        Math.random() * Math.PI,
-                        Math.random() * Math.PI,
-                    ]}
-                    position={[
-                        (Math.random() - 0.5) * 10,
-                        (Math.random() - 0.5) * 10,
-                        (Math.random() - 0.5) * 10,
-                    ]}
-                    scale={0.2 + Math.random() * 0.2}
-                />
-            ))}
+            <group ref={particlesGroup}>
+                {[...Array(100)].map((value, index) => (
+                    <Diamond
+                        key={index}
+                        rotation={[
+                            Math.random() * Math.PI,
+                            Math.random() * Math.PI,
+                            Math.random() * Math.PI,
+                        ]}
+                        position={[
+                            (Math.random() - 0.5) * 10,
+                            (Math.random() - 0.5) * 10,
+                            (Math.random() - 0.5) * 10,
+                        ]}
+                        scale={0.2 + Math.random() * 0.2}
+                    />
+                ))}
+            </group>
         </>
     );
 };
